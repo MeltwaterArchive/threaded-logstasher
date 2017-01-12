017259d172b38541d82f599142e23e613398ecc1
Győző Papp <gyozo.papp@meltwater.com>
2017-01-12 16:50:13 +0100

    improve setup.py a bit before publication

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
+import codecs
+import os.path
+
+def read(*parts):
+    return codecs.open(os.path.join(os.path.dirname(__file__), *parts),
+                       encoding="utf-8").read()
 
 if __name__ == '__main__':
    setup(name='logstasher',
-         version='0.1.4',
+         version='0.1.5',
          description='Threaded logstash handler and formatter',
+         long_description=read('README.md'),
          url='https://github.com/meltwater/threaded-logstasher',
          author='Gyozo Papp',
          author_email='gyozo.papp@meltwater.com',
          packages=find_packages(),
          install_requires=['logstash_formatter==0.5.14'],
+         keywords='logging logstash threaded',
          license='BSD',
          classifiers=[
           'Development Status :: 4 - Beta',
