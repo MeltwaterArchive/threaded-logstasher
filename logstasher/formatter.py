b7c846adaf2e49b1ecefba55ac4a99298f1006dc
Győző Papp <gyozo.papp@meltwater.com>
2017-01-12 15:15:47 +0100

    use keyword argument list in order to enforce compatibility w/ underlying logstash_formatter as much as possible

--- logstasher/formatter.py
+++ logstasher/formatter.py
@@ -7,8 +7,10 @@ class LogstashFormatterExtra(LogstashFormatterV1, object):
                  json_cls=None,
                  json_default=_default_json_default,
                  source_host=None, **extra):
-        super(LogstashFormatterExtra, self).__init__(None, datefmt, json_cls, json_default)
+        super(LogstashFormatterExtra, self).__init__(fmt=None,
+                                                     datefmt=datefmt,
+                                                     json_cls=json_cls,
+                                                     json_default=json_default)
         self.defaults.update(extra)
         if source_host:
             self.source_host = source_host
-
