35fd7bf7193591a3df19b348b1b43c31a4176bf6
gypapp <gyozo.papp@meltwater.com>
2017-01-12 14:06:45 +0100

    Update licence and classifiers
    preparing to make this package publicly available.

--- setup.py
+++ setup.py
@@ -10,5 +10,15 @@ if __name__ == '__main__':
          author_email='gyozo.papp@meltwater.com',
          packages=find_packages(),
          install_requires=['logstash_formatter==0.5.14'],
-         license=None,
+         license='BSD',
+         classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Natural Language :: English',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3'
+         ],
          zip_safe=False)
